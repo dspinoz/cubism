b0dc0c772872292d51e5475cf0aae4ad7ed7a985
Daniel Spinozzi <dspinoz@gmail.com>
2016-07-06 16:13:06 +0930

    use zero fill policy by default, but allow override as some aggregators return interesting results, eg. count

--- cubism.v1.js
+++ cubism.v1.js
@@ -551,22 +551,23 @@ cubism_contextPrototype.opentsdb = function(host) {
     var out = "";
     out += e.aggregator ? e.aggregator : "sum";
     out += ":";
+    // zero fill policy ensures that there is data for all step intervals
     if (e.downsample) {
       out += e.downsample.interval ? e.downsample.interval : step + "ms";
       out += "-";
       out += e.downsample.aggregator ? e.downsample.aggregator : "avg";
       out += "-";
-      out += e.downsample.fillpolicy ? e.downsample.fillpolicy : "none";
+      out += e.downsample.fillpolicy ? e.downsample.fillpolicy : "zero";
     }
     else {
-      out += step + "ms-avg-none";
+      out += step + "ms-avg-zero";
     }
     out += ":";
     out += e.metric ? e.metric : "metric";
     return out;
   }
       
-  source.metric = function(expression, title, dataCallback) {
+  source.metric = function(expression, title, dataCallback, fillCallback) {
     
     if (!dataCallback && typeof title === "function") {
       dataCallback = title;
@@ -591,32 +592,39 @@ cubism_contextPrototype.opentsdb = function(host) {
           
           data.forEach(function(d) { d.key = +d.key; });
           
+          // d3.entries returns undefined order
           var sorted = data
-            // ensure time sorted, oldest to newest
             .sort(function(a,b) {
               return d3.ascending(a.key, b.key);
             });
             
-          // interpolate additional data that is "missing"
-          var out = d3.range(start.valueOf()/1000, stop.valueOf()/1000, step/1000)
-            .map(function(d) { return {key:d, value:0}; })
-          
-          for (var i = 0, j = 0; i < out.length && j < sorted.length; i++) {
-            if (out[i].key == sorted[j].key) {
+          if (fillCallback) {
               
-              if (dataCallback) {
-                out[i].value = dataCallback(sorted[j]);
+            // interpolate additional data that is "missing"
+            var out = d3.range(start.valueOf()/1000, stop.valueOf()/1000, step/1000)
+              .map(function(d) { return {key:d, value:fillCallback()}; })
+            
+            for (var i = 0, j = 0; i < out.length && j < sorted.length; i++) {
+              if (out[i].key == sorted[j].key) {
+                
+                if (dataCallback) {
+                  out[i].value = dataCallback(sorted[j]);
+                }
+                else {
+                  out[i].value = sorted[j].value;
+                }
+                
+                out[i]._set = true;
+                j++;
               }
-              else {
-                out[i].value = sorted[j].value;
-              }
-              
-              out[i]._set = true;
-              j++;
             }
+            console.log(out.length, out.filter(function(d) { return d._set; }).length);
+            callback(null, out.map(function(d) { return d.value; }));
           }
-          
-          callback(null, out.map(function(d) { return d.value; }));
+          else
+          {
+            callback(null, sorted.map(function(d) { return d.value; }));
+          }
         });
     }, title);
   };
